fd5bd350c2a359b26b2bef6d9450677e7565372a
Fabian Rivera <frakxman@hotmail.com>
2021-02-16 21:33:20 -0500

    modules 'n' principal components generation

---
+++ src/app/country/country.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ByCityComponent } from './pages/by-city/by-city.component';
+import { ByCountyComponent } from './pages/by-county/by-county.component';
+import { ByRegionComponent } from './pages/by-region/by-region.component';
+import { SeeCountryComponent } from './pages/see-country/see-country.component';
+
+
+
+@NgModule({
+  declarations: [ByCityComponent, ByCountyComponent, ByRegionComponent, SeeCountryComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CountryModule { }
---
+++ src/app/country/pages/by-county/by-county.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-by-county',
+  templateUrl: './by-county.component.html',
+  styles: [
+  ]
+})
+export class ByCountyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
